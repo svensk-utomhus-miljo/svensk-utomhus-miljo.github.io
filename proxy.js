93da0539207266cd0cfa0a5652c825475c3be24f
Jimmy Wärting <jimmy@warting.se>
2025-04-26 00:20:34 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,16 +1,24 @@
 const map = new URLPattern('https://maps.googleapis.com/maps-api-v3/api/js/:someId/:version/intl/:lang/map.js')
 const common = new URLPattern('https://maps.googleapis.com/maps-api-v3/api/js/:someId/:version/intl/:lang/common.js')
-const getPlace = new URLPattern('https://places.googleapis.com/$rpc/google.maps.places.v1.Places/*')
+const getPlace = new URLPattern('https://*.googleapis.com/$rpc/*')
 const auth = new URLPattern('https://maps.googleapis.com/maps/api/js/AuthenticationService.Authenticate')
 
 const matcher = ctx => {
-  return map.exec(ctx.url) || common.exec(ctx.url) || getPlace.exec(ctx.url) || auth.exec(ctx.url)
+  // console.log('ctx.url', ctx.url+'')
+  return map.exec(ctx.url) ||
+    common.exec(ctx.url) ||
+    getPlace.exec(ctx.url) ||
+    auth.exec(ctx.url)
 }
 
 const get = c => {
   const q = new URLSearchParams({
     cors: JSON.stringify({
       ...c,
+      headers: {
+        origin: 'http://localhost:5173',
+        referer: 'http://localhost:5173/',
+      },
       forwardRequestHeaders: false
     })
   })
@@ -21,18 +29,7 @@ const handler = async ctx => {
   if (map.exec(ctx.url)) {
     let script = await get({ url: ctx.url })
 
-    var unknownStatusRegex = /const\s+(\w+)\s*=.*?;/g;
-    var unknownStatusMatch = script.match(unknownStatusRegex) || []
-
-    for (let i = 0; i < unknownStatusMatch.length; i++) {
-      if (unknownStatusMatch[i].includes('getStatus')) {
-        script = script.replace(
-          unknownStatusMatch[i],
-          unknownStatusMatch[i].replace(/=.*/, '=1;')
-        )
-        break
-      }
-    }
+    script = script.replace(new RegExp(/if\(\w+!==1&&\w+!==2\)/), "if(false)")
 
     return new Response(script, {
       headers: { 'Content-Type': 'application/javascript' }
@@ -74,6 +71,19 @@ const handler = async ctx => {
       })
     })
 
+    let body = null
+    try {
+      if (ctx.request.body) {
+        body = await ctx.request.arrayBuffer()
+        const decoder = new TextDecoder()
+        const text = decoder.decode(body)
+        if (text.includes(location.origin)) {
+          body = text.replaceAll(location.origin, 'http://localhost:5173')
+        }
+      }
+    } catch (e) {
+      // ignore
+    }
     return fetch('https://adv-cors.deno.dev/?' + q, {
       method: ctx.request.method,
       headers: ctx.request.headers,
@@ -88,17 +98,6 @@ const handler = async ctx => {
       headers: { 'Content-Type': 'application/javascript' }
     })
   }
-
-  return
-
-  const url = new URL('/api/cors', import.meta.url)
-  url.searchParams.set('url', ctx.url)
-
-  return fetch(url, {
-    method: ctx.request.method,
-    headers: ctx.request.headers,
-    body: ctx.request.body ? await ctx.request.arrayBuffer() : null,
-  })
 }
 
 export default [ matcher, handler ]
